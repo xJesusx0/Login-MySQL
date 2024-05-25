5ffb16fdacc8b44aab37f83af2bab337c3c55df1
jesus <jesuspl0411@gmail.com>
2024-05-25 12:50:42 -0500

    Crear cuenta funcionando correctamente

--- Models/models.py
+++ Models/models.py
@@ -7,6 +7,7 @@ def handle_database_operations(func):
 
         try:
             result = func(self, cursor, *args, **kwargs)
+            self.mysql.connection.commit()
             return result
         
         except Exception as error:
@@ -25,11 +26,26 @@ class Users():
     @handle_database_operations
     def validate_user(self, cursor, username, password):
 
-        cursor.execute("SELECT * FROM users WHERE name = %s AND password = %s", (username, password))
+        cursor.execute("SELECT * FROM users WHERE name = %s AND password = %s ;", (username, password))
         response = cursor.fetchone()
 
         if response:
             return response
         
         return None
+    
+    @handle_database_operations
+    def user_exists(self,cursor,username):
+        cursor.execute("SELECT * FROM users WHERE name = %s ;", (username,))
+        response = cursor.fetchone()
+
+        if response:
+            return True
         
+        return False
+    
+    @handle_database_operations
+    def register_user(self, cursor,username, password):
+        cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s);", (username, password))
+
+
--- app.py
+++ app.py
@@ -27,29 +27,61 @@ def index():
 @app.route("/login", methods=["POST","GET"])
 def login():
 
-    if request.method == "POST":
-
-        users = Users(mysql=mysql)
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        response = users.validate_user(username,password)
+    if request.method != "POST":
+        return render_template("login.html")
     
-        if response == None:
-            flash("Nombre de usuario o contraseña incorrectos")
-            return render_template("login.html")
+    users = Users(mysql=mysql)
 
-        session["name"] = username
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-        return redirect("/") 
+    response = users.validate_user(username,password)
     
-    return render_template("login.html")
+    if response == None:
+        flash("Nombre de usuario o contraseña incorrectos")
+        return render_template("login.html")
+
+    session["name"] = username
+    session["id"] = response[0]
+
+    return redirect("/")     
+    
+    
 
 @app.route("/logout",methods=["GET"])
 def logout():
     session["name"] = None
+    session["id"] = None
     return redirect("/login")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+
+    if request.method != "POST":
+        return render_template("register.html")
+    
+    username = request.form.get("username")  
+    confirm_username = request.form.get("confirm-username")
+
+    password = request.form.get("password")  
+    confirm_password = request.form.get("confirm-password")
+
+    if username != confirm_username:
+        flash("Los nombres de usuario no coinciden")
+        return render_template("register.html")
+
+    if password != confirm_password:
+        flash("Las contraseñas no coinciden")
+        return render_template("register.html")
+
+    users = Users(mysql=mysql)
+    
+    if users.user_exists(username):
+        flash("Usuario existente, intenta con otro")
+        return render_template("register.html")
+
+    users.register_user(username,password)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
